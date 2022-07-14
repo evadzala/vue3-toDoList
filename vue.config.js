0f96948f167935a1b36bae097d33c3a428aeb4f1
chiuaslan0620@gmail.com <chiuaslan0620@gmail.com>
2022-07-14 17:04:19 +0800

    修改路徑

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/API2/'
-    : '/'
 })
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/API2/'
+    ? '/vue3-toDoList/'
     : '/'
 }
